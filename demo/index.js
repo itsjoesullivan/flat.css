72989c1bb36fb2e39242a69d3a0835d71468a612
itsjoesullivan <itsjoesullivan@gmail.com>
2013-06-19 22:40:13 -0400

    script

---
+++ demo/index.js
@@ -0,0 +1,10 @@
+var elements = document.getElementsByTagName('a');
+for(var i = 1; i < elements.length; i++) {
+    elements[i].onclick = function() {
+        var hash = this.hash.substr(1),
+            elementTop = document.getElementById(hash).offsetTop;
+        window.scrollTo(0, elementTop + 125);
+        window.location.hash = '';
+        return false;
+    }
+}
